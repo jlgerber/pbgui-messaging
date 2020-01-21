ca67fb56f3d18979c4022575f92fefaf4be336af
jlgerber <jlgerber@gmail.com>
2020-01-21 14:24:15 -0800

    split out vpin related message into own modules

--- src/event_handler.rs
+++ src/event_handler.rs
@@ -6,6 +6,44 @@ use qt_core::{QString, SlotOfQString};
 use qt_widgets::cpp_core::Ref;
 use std::rc::Rc;
 
+// TODO: move to event_handler/vpin_dialog.rs
+pub(crate) mod vpin_dialog_event_handler {
+    use super::*;
+
+    pub fn match_vpin_dialog<'a>(
+        event: VpinDialog,
+        dialog: Rc<vpin_dialog::VpinDialog<'a>>,
+        receiver: &Receiver<IMsg>,
+    ) {
+        match event {
+            VpinDialog::UpdateSites => {
+                if let Ok(IMsg::VpinDialog(IVpinDialog::Sites(sites))) = receiver.recv() {
+                    let sites_ref = sites.iter().map(|x| x.as_str()).collect::<Vec<_>>();
+                    dialog.set_sites(sites_ref);
+                } else {
+                    log::error!("Event::UpdateSites IMsg does not match event state");
+                }
+            }
+            VpinDialog::UpdateRoles => {
+                if let Ok(IMsg::VpinDialog(IVpinDialog::Roles(roles))) = receiver.recv() {
+                    let roles_ref = roles.iter().map(|x| x.as_str()).collect::<Vec<_>>();
+                    dialog.set_roles(roles_ref);
+                } else {
+                    log::error!("IMsg does not have Roles")
+                }
+            }
+            VpinDialog::UpdateLevels => {
+                if let Ok(IMsg::VpinDialog(IVpinDialog::Levels(level_map))) = receiver.recv() {
+                    dialog.set_levels(level_map);
+                } else {
+                    log::error!("IMsg does not have LevelMap");
+                }
+            }
+        }
+    }
+}
+use vpin_dialog_event_handler::match_vpin_dialog;
+
 /// Generate a new event handler, which is of type `SlotOfQString`.
 /// The event handler is responsible for handling Signals of type Event
 ///
@@ -20,28 +58,8 @@ pub fn new_event_handler<'a>(
     receiver: Receiver<IMsg>,
 ) -> SlotOfQString<'a> {
     SlotOfQString::new(move |name: Ref<QString>| match Event::from_qstring(name) {
-        Event::VpinDialog(VpinDialog::UpdateSites) => {
-            if let Ok(IMsg::VpinDialog(IVpinDialog::Sites(sites))) = receiver.recv() {
-                let sites_ref = sites.iter().map(|x| x.as_str()).collect::<Vec<_>>();
-                dialog.set_sites(sites_ref);
-            } else {
-                log::error!("Event::UpdateSites IMsg does not match event state");
-            }
-        }
-        Event::VpinDialog(VpinDialog::UpdateRoles) => {
-            if let Ok(IMsg::VpinDialog(IVpinDialog::Roles(roles))) = receiver.recv() {
-                let roles_ref = roles.iter().map(|x| x.as_str()).collect::<Vec<_>>();
-                dialog.set_roles(roles_ref);
-            } else {
-                log::error!("IMsg does not have Roles")
-            }
-        }
-        Event::VpinDialog(VpinDialog::UpdateLevels) => {
-            if let Ok(IMsg::VpinDialog(IVpinDialog::Levels(level_map))) = receiver.recv() {
-                dialog.set_levels(level_map);
-            } else {
-                log::error!("IMsg does not have LevelMap");
-            }
+        Event::VpinDialog(vpin_dialog_event) => {
+            match_vpin_dialog(vpin_dialog_event, dialog.clone(), &receiver)
         }
         Event::Error => {
             if let Ok(IMsg::Error(error)) = receiver.recv() {
